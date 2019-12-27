6437328744f53987695db1ef6f04bd65d368f129
david <david@acriil.com>
2019-12-27 14:54:26 +0900

    비밀번호 해쉬 암호화 기능 추가 및 회원가입 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column
 
@@ -14,14 +15,27 @@ class User(db.Model):
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
 
     uid = Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
+    email = Column(db.String(100), nullable=False)
     password = Column(db.String(100), nullable=False)
-    first_name = Column(db.String(100), nullable=False)
-    last_name = Column(db.String(100), nullable=False)
-    email = Column(db.String(100), nullable=True)
+    first_name = Column(db.String(100), nullable=True)
+    last_name = Column(db.String(100), nullable=True)
     gender = Column(db.String(100), nullable=False)
+
+    def __init__(self, email, password, gender, first_name=None, last_name=None):
+        self.email = email
+        self.set_password(password)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
 
     def __repr__(self):
         return f"<User('{self.uid}', '{self.first_name}', '{self.email}')>"
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class Test(db.Model):
@@ -46,24 +60,46 @@ def join():
     return render_template('user/join.html')
 
 
+@app.route('/user/join', methods=["POST"])
+def setJoin():
+    req = request.form
+    email = req.get("email")
+    password = req.get("password")
+    first_name = req.get("first_name", None)
+    last_name = req.get("last_name", None)
+    gender = req.get("gender")
+
+
+    userCount = User.query.filter_by(email=email).count()
+    if userCount == 0:
+        newUser = User(email=email, password=password, first_name=first_name, last_name=last_name, gender=gender)
+        db.session.add(newUser)
+        db.session.commit()
+
+        return render_template('user/login.html', email=email)
+    else:
+        return render_template('user/join.html', errorMsg="존재하는 이메일 입니다.")
+
+
+
 @app.route("/user/login", methods=["POST"])
 def checkUser():
-    if request.method == 'POST':
-        req = request.form
+    # if request.method == 'POST':
+    req = request.form
 
-        # 데이터 받기
-        email = req.get("email")
-        pw = req.get("password")
+    # 데이터 받기
+    email = req.get("email")
+    pw = req.get("password")
 
-        # 서버 연결
-        rowCount = User.query.filter_by(email=email, password=pw).count()
-        row = None
-        if rowCount == 0:
-            row = User.query.all()
-        else:
-            row = User.query.filter_by(email=email, password=pw)
+    # 서버 연결
+    rowCount = User.query.filter_by(email=email, password=pw).count()
+    row = None
+    if rowCount == 0:
+        row = User.query.all()
+    else:
+        row = User.query.filter_by(email=email, password=pw)
 
-        return render_template('user/show.html', row=row)
+    return render_template('user/show.html', row=row)
 
 
 if __name__ == '__main__':
